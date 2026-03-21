187292c2a6a7d64188ebb9f70af981549587fe1b
mikethetitanium <mikejohnteh@gmail.com>
2026-03-21 12:01:29 +0300

    Initial setup from spring boot initializer. Added pdf depenedency to pom.xml as well. I will not put anything to git ignore as I want you to be able to download and run the project on the fly. This is also the reason I have used h2 database which runs in memory

---
+++ src/main/java/com/FS3Malta/FS3Malta/Fs3MaltaApplication.java
@@ -0,0 +1,13 @@
+package com.FS3Malta.FS3Malta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fs3MaltaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Fs3MaltaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/FS3Malta/FS3Malta/Fs3MaltaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FS3Malta.FS3Malta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Fs3MaltaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
